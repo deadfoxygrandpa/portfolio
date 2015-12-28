fb0cf595f4757c449a314a688b9a8b2d198b7cd2
Alex Neslusan <deadfoxygrandpa@gmail.com>
2015-12-28 22:56:44 +0800

    Start new design

--- src/Main.elm
+++ src/Main.elm
@@ -1,170 +1,57 @@
-module Main where
+module Main (..) where
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-import Graphics.Element exposing (..)
-import Color
-
-import StartApp.Simple as StartApp
+import Effects exposing (..)
+import Task
+import StartApp
 
 
--- design inspired by http://www.thomsoon.com/thomsoon/projects.html
+app : StartApp.App Model
+app =
+    StartApp.start { init = ( model, none ), view = view, update = update, inputs = [] }
 
 
+main : Signal Html
 main =
-  StartApp.start { model = model, view = view, update = update }
+    app.html
 
+
+port tasks : Signal (Task.Task Effects.Never ())
+port tasks =
+    app.tasks
 
 
 port title : String
 port title =
-  "Gigi's Portfolio?"
+    "Gg"
 
 
-model = 
-  False
+type Model
+    = All
+    | Interface
+    | UX
+    | Graphic
+    | Photograph
 
 
+model : Model
+model =
+    All
+
+
+type Action
+    = Click Model
+
+
+update : Action -> a -> ( Model, Effects Action )
 update action model =
-  case action of
-    _ -> not model
+    case action of
+        Click model' ->
+            ( model', none )
 
 
+view : Signal.Address Action -> Model -> Html
 view address model =
-  div 
-    [ style 
-      [ ("fontFamily", "\"Raleway\", sans-serif") 
-      , ("color", "black")
-      , ("position", "relative")
-      ] 
-    ] 
-    [ hamburger address model
-    , fullscreenMenu address model
-    , logo
-    , body
-    , footer
-    , node "link"
-      [ rel "stylesheet"
-      , href "https://fonts.googleapis.com/css?family=Raleway:400,700,900,800"
-      , type' "text/css"
-      ]
-      []
-    ]
-
-
-logo =
-  div 
-    [ style 
-      [ ("cursor", "pointer") 
-      , ("fontSize", "20px")
-      , ("fontWeight", "800")
-      , ("lineHeight", "80px")
-      , ("width", "400px")
-      , ("margin", "0px auto")
-      ] 
-    ] 
-    [ text "GIGI"
-    , spacer 150 1 |> color Color.lightGrey |> fromElement
-    , node "link" 
-      [ rel "stylesheet"
-      , href "https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"
-      ]
-      []
-    ]
-    
-
-body =
-  div 
-    [ style 
-      [ ("fontSize", "13px")
-      , ("color", "#8D8D8D")
-      , ("lineHeight", "22px")
-      , ("letterSpacing", "1px")
-      , ("width", "400px")
-      , ("margin", "50px auto")
-      ]
-    ] 
-    [ h1 
-      [ style 
-        [ ("fontSize", "43px")
-        , ("fontWeight", "900")
-        , ("lineHeight", "70px")
-        , ("color", "black")
-        , ("letterSpacing", "0px")
-        ]
-      ] 
-      [ text "PROJECTS" ]
-    , text "Please check my portfolio."
-    , pic Color.grey
-    , pic Color.lightGrey
-    , pic Color.grey
-    ]
-
-
-hamburger address model =
-  node
-    "i"
-    [ class <| if model then "fa fa-close" else "fa fa-bars"
-    , style 
-      [ ("backgroundColor", "black") 
-      , ("position", "fixed")
-      , ("right", "5%")
-      , ("top", "30px")
-      , ("padding", "20px")
-      , ("color", "white")
-      , ("fontSize", "18px")
-      , ("cursor", "pointer")
-      , ("zIndex", "9999")
-      ]
-    , onClick address ()
-    ]
-    []
-    
-
-fullscreenMenu address model =
-  if model then
     div
-      [ style
-        [ ("position", "fixed")
-        , ("height", "100%")
-        , ("width", "100%")
-        , ("background", "rgba(0, 0, 0, 0.95)")
-        , ("zIndex", "9998")
-        ]
-      ]
-      [ text "welp," ]
-  else
-    div [] []
-
-
-footer =
-  div
-    [ style 
-      [ ("height", "300px")
-      , ("marginTop", "150px")
-      , ("background", "#F1F1F1")
-      , ("color", "#686868")
-      , ("letterSpacing", "1px")
-      , ("fontSize", "12px")
-      , ("textAlign", "center")
-      ]
-    ]
-    [ divSpacer 150
-    , text "© Copyright 2015 deadfoxygrandpa.xyz. All Rights Reserved." 
-    ]
-
-
-pic c =
-  p [] [ spacer 360 305 |> color c |> fromElement ]
-  
-  
-divSpacer h =
-  div
-    [ style
-      [ ("height", toString h ++ "px")
-      , ("width", "100%")
-      ]
-    ]
-    []
-  
+        []
+        [ text "welp," ]
