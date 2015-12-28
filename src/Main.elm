addfdca3f7bd0f0947c27af765167a56c3451afb
Alex Neslusan <deadfoxygrandpa@gmail.com>
2015-12-29 00:14:36 +0800

    Add logo, title, and menu icon

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,11 @@ module Main (..) where
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Effects exposing (..)
+import Svg
+import Material.Icons.Social
+import Material.Icons.Navigation
 import Task
+import Color
 import StartApp
 
 
@@ -54,10 +58,74 @@ update action model =
 view : Signal.Address Action -> Model -> Html
 view address model =
     div
-        []
-        [ text "welp," ]
+        [ style
+            [ "color" => "#50e3c2"
+            , "fontFamily" => "sans-serif"
+            ]
+        ]
+        [ logo
+        , title'
+        , hamburger
+        ]
 
 
 (=>) : a -> b -> ( a, b )
 (=>) a b =
     ( a, b )
+
+
+logo : Html
+logo =
+    div
+        []
+        [ div
+            [ style
+                [ "position" => "absolute"
+                , "top" => "46px"
+                , "left" => "51px"
+                ]
+            ]
+            [ Svg.svg
+                []
+                [ Material.Icons.Social.public (Color.rgb 248 231 28) 50 ]
+            ]
+        , div
+            [ style
+                [ "position" => "absolute"
+                , "top" => "55.8px"
+                , "left" => "94.6px"
+                ]
+            ]
+            [ Svg.svg
+                []
+                [ Material.Icons.Social.public (Color.rgb 80 227 194) 50 ]
+            ]
+        ]
+
+
+title' : Html
+title' =
+    h1
+        [ style
+            [ "top" => "41px"
+            , "textAlign" => "center"
+            ]
+        ]
+        [ text "PROJECTS" ]
+
+
+hamburger : Html
+hamburger =
+    div
+        [ style
+            [ "position" => "fixed"
+            , "top" => "61px"
+            , "right" => "50px"
+            , "width" => "50px"
+            , "height" => "30px"
+            ]
+        ]
+        [ Svg.svg
+            []
+            [ Material.Icons.Navigation.menu (Color.rgb 74 74 74) 50 ]
+        ]
