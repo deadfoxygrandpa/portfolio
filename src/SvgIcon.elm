ec47b4bea6249d39bb976264eafe0d059552f4c3
Alex Neslusan <deadfoxygrandpa@gmail.com>
2016-01-07 17:11:16 +0800

    Add Gg icon

---
+++ src/SvgIcon.elm
@@ -0,0 +1,75 @@
+module SvgIcon (icon, icon2) where
+
+import Color exposing (Color)
+import Svg exposing (Svg)
+import Svg.Attributes
+
+
+{- Copied from https://github.com/elm-community/elm-material-icons -}
+
+
+icon : String -> Color -> Int -> Int -> Svg
+icon path color w h =
+    let
+        stringWidth = toString w
+
+        stringHeight = toString h
+
+        stringColor = toRgbaString color
+    in
+        Svg.svg
+            [ Svg.Attributes.width stringWidth
+            , Svg.Attributes.height stringHeight
+            , Svg.Attributes.viewBox "0 0 80 60"
+            ]
+            [ Svg.path
+                [ Svg.Attributes.d path
+                , Svg.Attributes.fill stringColor
+                ]
+                []
+            ]
+
+
+icon2 : String -> String -> Color -> Color -> Int -> Int -> Svg
+icon2 path1 path2 color1 color2 w h =
+    let
+        stringWidth = toString w
+
+        stringHeight = toString h
+
+        stringColor1 = toRgbaString color1
+
+        stringColor2 = toRgbaString color2
+    in
+        Svg.svg
+            [ Svg.Attributes.width stringWidth
+            , Svg.Attributes.height stringHeight
+            , Svg.Attributes.viewBox "0 0 80 60"
+            ]
+            [ Svg.path
+                [ Svg.Attributes.d path1
+                , Svg.Attributes.fill stringColor1
+                ]
+                []
+            , Svg.path
+                [ Svg.Attributes.d path2
+                , Svg.Attributes.fill stringColor2
+                ]
+                []
+            ]
+
+
+toRgbaString : Color -> String
+toRgbaString color =
+    let
+        { red, green, blue, alpha } = Color.toRgb color
+    in
+        "rgba("
+            ++ toString red
+            ++ ","
+            ++ toString green
+            ++ ","
+            ++ toString blue
+            ++ ","
+            ++ toString alpha
+            ++ ")"
