82b6e10a3cbdb540a4c9541ca7cf32939fbfbc67
Alex Neslusan <deadfoxygrandpa@gmail.com>
2016-02-11 22:25:18 +0800

    Add header clicking

--- src/Header.elm
+++ src/Header.elm
@@ -2,6 +2,7 @@ module Header (..) where
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events
 import Effects
 import Svg
 import Svg.Attributes
@@ -13,60 +14,85 @@ import Graphics.Collage as Collage
 
 (=>) : a -> b -> ( a, b )
 (=>) a b =
-    ( a, b )
+  ( a, b )
 
 
 type alias Model =
-    { title : String
-    , menu : Menu.Model
-    }
+  { title : String
+  , menu : Menu.Model
+  , language : Language
+  }
+
+
+type Language
+  = English
+  | Chinese
 
 
 type Action
-    = MenuAction Menu.Action
+  = ChangeLanguage Language
+  | MenuAction Menu.Action
 
 
 init : String -> Model
 init title =
-    Model title Menu.init
+  Model title Menu.init English
 
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-    Html.header
-        []
-        [ logo
-        , title' model.title
-        , Menu.view (Signal.forwardTo address MenuAction) model.menu
-        ]
+  Html.header
+    []
+    [ languageSelector address model.language
+    , title' model.title
+    , Menu.view (Signal.forwardTo address MenuAction) model.menu
+    ]
 
 
 update : Action -> Model -> ( Model, Effects.Effects Action )
 update action model =
-    case action of
-        MenuAction menuAction ->
-            let
-                ( newMenu, effect ) = Menu.update menuAction model.menu
-            in
-                ( { model | menu = newMenu }, Effects.map MenuAction effect )
+  case action of
+    ChangeLanguage language ->
+      ( { model | language = language }, Effects.none )
+
+    MenuAction menuAction ->
+      let
+        ( newMenu, effect ) =
+          Menu.update menuAction model.menu
+      in
+        ( { model | menu = newMenu }, Effects.map MenuAction effect )
 
 
-logo : Html
-logo =
-    Html.div
-        []
-        [ Html.text "English"
-        , Html.text "|"
-        , Html.text "中文"
+languageSelector : Signal.Address Action -> Language -> Html
+languageSelector address language =
+  Html.div
+    [ Html.Attributes.class "languageselector" ]
+    [ Html.span
+        [ Html.Attributes.classList
+            [ "language" => True
+            , "selectedlanguage" => (language == English)
+            ]
+        , Html.Events.onClick address (ChangeLanguage English)
         ]
+        [ Html.text "English" ]
+    , Html.span [ Html.Attributes.class "language" ] [ Html.text "|" ]
+    , Html.span
+        [ Html.Attributes.classList
+            [ "language" => True
+            , "selectedlanguage" => (language == Chinese)
+            ]
+        , Html.Events.onClick address (ChangeLanguage Chinese)
+        ]
+        [ Html.text "中文" ]
+    ]
 
 
 title' : String -> Html
 title' title =
-    h1
-        []
-        [ text title ]
+  h1
+    []
+    [ text title ]
 
 
 sub =
-    Collage.collage 300 300 []
+  Collage.collage 300 300 []
--- src/Menu.elm
+++ src/Menu.elm
@@ -13,127 +13,127 @@ import Json.Decode as Json
 
 
 type Action
-    = Close
-    | Open
-    | Toggle
+  = Close
+  | Open
+  | Toggle
 
 
 type alias Model =
-    { open : Bool }
+  { open : Bool }
 
 
 (=>) : a -> b -> ( a, b )
 (=>) a b =
-    ( a, b )
+  ( a, b )
 
 
 clickTo : String -> List Attribute
 clickTo path =
-    [ href path
-    ]
+  [ href path
+  ]
 
 
 init : Model
 init =
-    { open = False }
+  { open = False }
 
 
 update : Action -> Model -> ( Model, Effects.Effects Action )
 update action model =
-    case action of
-        Close ->
-            ( { model | open = False }, Effects.none )
+  case action of
+    Close ->
+      ( { model | open = False }, Effects.none )
 
-        Open ->
-            ( { model | open = True }, Effects.none )
+    Open ->
+      ( { model | open = True }, Effects.none )
 
-        Toggle ->
-            ( { model | open = not model.open }, Effects.none )
+    Toggle ->
+      ( { model | open = not model.open }, Effects.none )
 
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-    div
-        []
-        [ Html.Lazy.lazy2 hamburger address ( model.open, 0 )
-        , if model.open then
-            menu address model
-          else
-            div [] []
-        ]
+  div
+    []
+    [ Html.Lazy.lazy2 hamburger address ( model.open, 0 )
+    , if model.open then
+        menu address model
+      else
+        div [] []
+    ]
 
 
 menu : Signal.Address Action -> Model -> Html
 menu address model =
-    let
-        makeLink heading string ref target =
-            heading [] [ Html.a (clickTo ref) [ text string ] ]
-    in
-        Html.div
-            [ Html.Attributes.class "menu"
-            ]
-            [ Html.div
-                [ Html.Attributes.classList
-                    [ "flex-container" => True
-                    , "centered-column" => True
-                    , "screen-center" => True
-                    ]
-                  --, Html.Attributes.style
-                  --    [ "position" => "fixed"
-                  --    , "top" => "50%"
-                  --    , "left" => "50%"
-                  --    , "transform" => "translate(-50%, -60%)"
-                  --    ]
-                ]
-                [ makeLink Html.h1 "HOME" "#home" ""
-                , makeLink Html.h1 "PROJECTS" "#projects" ""
-                , makeLink Html.h1 "ABOUT ME" "#about" ""
-                , makeLink Html.h1 "CONTACT" "#contact" ""
-                ]
-            , Html.div
-                [ style
-                    [ "position" => "fixed"
-                    , "bottom" => "3em"
-                    , "left" => "50%"
-                    , "transform" => "translateX(-50%)"
-                    ]
-                ]
-                [ Html.h2
-                    []
-                    [ text "FOLLOW ME" ]
-                , Html.div
-                    [ Html.Attributes.style
-                        [ "display" => "inline-block"
-                        ]
-                    ]
-                    [ makeLink Html.h2 "Facebook" "https://www.facebook.com/profile.php?id=100002934303779" "_blank"
-                    , Html.h2 [] [ text "|" ]
-                    , makeLink Html.h2 "Sina Weibo" "http://www.weibo.com/u/1829414713?wvr=5&wvr=5&lf=reg" "_blank"
-                    , Html.h2 [] [ text "|" ]
-                    , makeLink Html.h2 "Instagram" "https://www.instagram.com/gigiguo80/" "_blank"
-                    , Html.h2 [] [ text "|" ]
-                    , makeLink Html.h2 "Behance" "https://www.behance.net/GigiGUO" "_blank"
-                    ]
-                ]
-            ]
+  let
+    makeLink heading string ref target =
+      heading [] [ Html.a (clickTo ref) [ text string ] ]
+  in
+    Html.div
+      [ Html.Attributes.class "menu"
+      ]
+      [ Html.div
+          [ Html.Attributes.classList
+              [ "flex-container" => True
+              , "centered-column" => True
+              , "screen-center" => True
+              ]
+            --, Html.Attributes.style
+            --    [ "position" => "fixed"
+            --    , "top" => "50%"
+            --    , "left" => "50%"
+            --    , "transform" => "translate(-50%, -60%)"
+            --    ]
+          ]
+          [ makeLink Html.h1 "HOME" "#home" ""
+          , makeLink Html.h1 "PROJECTS" "#projects" ""
+          , makeLink Html.h1 "ABOUT ME" "#about" ""
+          , makeLink Html.h1 "CONTACT" "#contact" ""
+          ]
+      , Html.div
+          [ style
+              [ "position" => "fixed"
+              , "bottom" => "3em"
+              , "left" => "50%"
+              , "transform" => "translateX(-50%)"
+              ]
+          ]
+          [ Html.h2
+              []
+              [ text "FOLLOW ME" ]
+          , Html.div
+              [ Html.Attributes.style
+                  [ "display" => "inline-block"
+                  ]
+              ]
+              [ makeLink Html.h2 "Facebook" "https://www.facebook.com/profile.php?id=100002934303779" "_blank"
+              , Html.h2 [] [ text "|" ]
+              , makeLink Html.h2 "Sina Weibo" "http://www.weibo.com/u/1829414713?wvr=5&wvr=5&lf=reg" "_blank"
+              , Html.h2 [] [ text "|" ]
+              , makeLink Html.h2 "Instagram" "https://www.instagram.com/gigiguo80/" "_blank"
+              , Html.h2 [] [ text "|" ]
+              , makeLink Html.h2 "Behance" "https://www.behance.net/GigiGUO" "_blank"
+              ]
+          ]
+      ]
 
 
 hamburger : Signal.Address Action -> ( Bool, Int ) -> Html
 hamburger address ( open, yOffset ) =
-    let
-        icon =
-            if open then
-                Material.Icons.Navigation.close (Color.rgb 255 255 255)
-            else
-                Material.Icons.Navigation.menu (Color.rgb 74 74 74)
-    in
-        div
-            [ Html.Attributes.class "cursor"
-            , onClick address Toggle
-            ]
-            [ Svg.svg
-                [ Svg.Attributes.width "50px"
-                , Svg.Attributes.height "50px"
-                ]
-                [ icon 50 ]
-            ]
+  let
+    icon =
+      if open then
+        Material.Icons.Navigation.close (Color.rgb 255 255 255)
+      else
+        Material.Icons.Navigation.menu (Color.rgb 74 74 74)
+  in
+    div
+      [ Html.Attributes.class "menutoggle"
+      , onClick address Toggle
+      ]
+      [ Svg.svg
+          [ Svg.Attributes.width "50px"
+          , Svg.Attributes.height "50px"
+          ]
+          [ icon 50 ]
+      ]
