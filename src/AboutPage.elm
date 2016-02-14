b15c33fd954c9464ae907e257c0072240be23e98
Alex Neslusan <deadfoxygrandpa@gmail.com>
2016-02-14 15:18:34 +0800

    Redesign About Me page

--- src/AboutPage.elm
+++ src/AboutPage.elm
@@ -5,19 +5,20 @@ import Html.Attributes
 import Effects
 import ColorScheme
 import Header
+import Markdown
 
 
 -- Model
 
 
 type alias Model =
-    { header : Header.Model }
+  { header : Header.Model }
 
 
 init : Model
 init =
-    Model
-        (Header.init "ABOUT ME")
+  Model
+    (Header.init "ABOUT ME")
 
 
 
@@ -25,17 +26,18 @@ init =
 
 
 type Action
-    = HeaderAction Header.Action
+  = HeaderAction Header.Action
 
 
 update : Action -> Model -> ( Model, Effects.Effects Action )
 update action model =
-    case action of
-        HeaderAction headerAction ->
-            let
-                ( newHeader, fx ) = Header.update headerAction model.header
-            in
-                ( { model | header = newHeader }, Effects.map HeaderAction fx )
+  case action of
+    HeaderAction headerAction ->
+      let
+        ( newHeader, fx ) =
+          Header.update headerAction model.header
+      in
+        ( { model | header = newHeader }, Effects.map HeaderAction fx )
 
 
 
@@ -44,89 +46,94 @@ update action model =
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-    Html.div
-        []
-        [ Html.div
-            [ Html.Attributes.style
-                [ "position" => "fixed"
-                , "top" => "0px"
-                , "left" => "0px"
-                , "zIndex" => "-999"
-                , "width" => "100%"
-                , "height" => "100%"
-                , "backgroundColor" => "#ebebeb"
-                ]
+  Html.div
+    []
+    [ Html.div
+        [ Html.Attributes.style
+            [ "position" => "fixed"
+            , "top" => "0px"
+            , "left" => "0px"
+            , "zIndex" => "-999"
+            , "width" => "100%"
+            , "height" => "100%"
+            , "backgroundColor" => "#ebebeb"
             ]
-            []
-        , Header.view (Signal.forwardTo address HeaderAction) model.header
-        , Html.div
-            [ Html.Attributes.style
-                [ "width" => "1100px"
+        ]
+        []
+    , Header.view (Signal.forwardTo address HeaderAction) model.header
+    , Html.div
+        [ Html.Attributes.style
+            [ "width" => "1100px"
+            , "position" => "relative"
+            , "left" => "50%"
+            , "transform" => "translateX(-50%)"
+            ]
+        ]
+        [ bar
+        , spacer "2.5em"
+        , Html.img
+            [ Html.Attributes.src "assets/gigi1.jpg"
+            , Html.Attributes.style
+                [ "width" => "100%"
+                , "height" => "550px"
                 , "position" => "relative"
                 , "left" => "50%"
                 , "transform" => "translateX(-50%)"
                 ]
             ]
-            [ spacer "4em"
-            , bar
-            , Html.h1
-                [ Html.Attributes.style
-                    [ "textAlign" => "center"
-                    , "fontWeight" => "200"
-                    , "color" => "#4a4a4a"
-                    ]
+            []
+        , Html.h1
+            [ Html.Attributes.style
+                [ "textAlign" => "center"
+                , "position" => "relative"
+                , "top" => "-2.5em"
+                , "color" => "#FFFFFF"
                 ]
-                [ Html.text "THANKS FOR VISITING" ]
-            , bar
-            , spacer "3.5em"
-            , Html.img
-                [ Html.Attributes.src "assets/gigi1.jpg"
-                , Html.Attributes.style
-                    [ "width" => "100%"
-                    , "height" => "550px"
-                    , "position" => "relative"
-                    , "left" => "50%"
-                    , "transform" => "translateX(-50%)"
-                    ]
+            ]
+            [ Html.text "THANKS FOR VISITING" ]
+        , Html.span
+            [ Html.Attributes.style
+                [ "color" => "#000000"
+                , "fontSize" => "1em"
+                , "position" => "relative"
+                , "top" => "-5em"
                 ]
-                []
-            , spacer "3.5em"
-            , Html.span
-                [ Html.Attributes.style
-                    [ "color" => "#000000"
-                    , "fontSize" => "1em"
-                    ]
-                ]
-                [ Html.text
-                    """Hi, I am Gigi, User experience & Graphic designer based in Shenzhen, China.
+            ]
+            [ textContent ]
+        ]
+    ]
+
+
+textContent : Html.Html
+textContent =
+  Markdown.toHtml
+    """Hi, I am Gigi, User experience & Graphic designer based in Shenzhen, China.
 I create a more useful and beautiful product from the UIUX skills that I had during serveral years of experience.
 
 Nice to meet you!
+
 I am a passionate girl both in life and work. It's not just because I am always very energetic but also because I love and enjoy my life at every moment.
 I grow up in a small town in Guangzhou, China, which helps me to keep myself close to nature/local environment. After studing in big cities for 7 years, I still think the love of culture and nature from childhood keep pushing me forward.
 
 Travelling is the way to know myself better, what I experience from the trips makes me think outside of the box and see myself and the world differently. That helps me to keep a fresh mind on design jobs and daily life, as well as continually extending my horizon of the world."""
-                ]
-            ]
-        ]
 
 
 bar : Html
 bar =
-    Html.div
-        [ Html.Attributes.class "bar" ]
-        []
+  Html.div
+    [ Html.Attributes.class "bar" ]
+    []
 
 
 spacer : String -> Html
 spacer size =
-    Html.div
-        [ Html.Attributes.class "spacer"
-        , Html.Attributes.style [ "height" => size ]
-        ]
-        []
+  Html.div
+    [ Html.Attributes.class "spacer"
+    , Html.Attributes.style [ "height" => size ]
+    ]
+    []
 
 
 (=>) : a -> b -> ( a, b )
 (=>) a b =
-    ( a, b )
+  ( a, b )
